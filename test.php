bcf8a200d2ebe636329e6fb9e921c4c995bb619a
Prapaline <morgane.harre@gmail.com>
2023-10-26 12:24:48 +0200

    Ajout du menu de sophie

--- test.php
+++ test.php
@@ -41,7 +41,7 @@ class Heros extends Personnage
     public function afficherAvantage(){
         return $this->avantage;
     }
-    public function attaquer(Vilain $cible){
+    public function attaquer(Mechants $cible){
         $degats = $this->afficherPuissance();
         $cible->subirDegats($degats);
     }
@@ -57,7 +57,7 @@ class Heros extends Personnage
     
 }
 //Creation de la classe vilain avec pour parent la classe personnage
-class Vilain extends Personnage
+class Mechants extends Personnage
 {
     //Définition des variables
     private $destructeur;
@@ -79,14 +79,80 @@ class Vilain extends Personnage
     public function afficherDestructeur(){
         return $this->destructeur;
     }
-    
 }
 //Création d'un héro
 $heros = new Heros("Jean", 100, 800, "toujours plus vite");
-$vilain = new Vilain("Janne", 100, 100, "Lance des bombes");
+$mechant = new Mechants("Janne", 100, 100, "Lance des bombes");
 
-$heros->attaquer($vilain);
+$heros->attaquer($mechant);
 
-echo $vilain->afficherSante();
+echo $mechant->afficherSante();
 
+//Création d'un héro
+
+//$heros=new Heros($nom,$puissance,$pointdevie,$avantage);
+
+$Goku=new Heros("Goku", "20 points de dégats", 100, "Bouclier");
+$Vegeta = new Heros("Vegeta", "10 points de dégats", 120, "plus de vie");
+
+//$vilain= new Mechants($nom, $puissance,$pointdevie,$destructeur);
+
+$Freezer = new Mechants("Freezer", "10 points de dégats", 140, "plus de vie");
+$Cell = new Mechants("Cell", "15 points de dégats", 100, "plus de dégat");
+
+
+
+echo "Bonjour à tous, et bienvenue sur 'Dragon Ball Game'\n
+    Nous sommes heureuses de vous accueillir pour un combat opposant les Héros et les Méchants de cette série culte.\n
+    Le jeu comporte au minimum 2 joueurs. Le but est de remporté le plus de combat possible pour gagner le jeu.\n
+    Bonne chance à tous et n'oubliez pas, 'Les limites existent uniquement si tu le permets'\n";
+
+$Commence=readline("Voulez-vous commencer (Oui / Non)? ");
+function Menu($Commence){
+    
+    if ($Commence== "Oui"){
+        echo "\nBienvenue dans le menu du jeu. Que voulez-vous faire ?\n
+    1. Jouer\n
+    2. Afficher les règles\n
+    3. Découvrir les personnages\n
+    4. Quitter le jeu\n";
+
+    $choix=readline("Quel est votre choix ?\n");
+        switch ($choix) {
+            case "1":
+                echo"Jouer";
+                break;
+            case "2":
+                echo "Le jeu comporte au minimum 2 joueurs. Le but est de remporté le plus de combat possible pour gagner le jeu.\n
+Bonne chance à tous et n'oubliez pas, 'Les limites existent uniquement si tu le permets'\n";
+                $revenir_menu=readline("Pour revenir sur le Menu merci de taper 'Go' : \n");
+                if ($revenir_menu== "Go"){
+                    popen("cls", "w");
+                    Menu($Commence);
+                }
+                break;
+            case "3":
+                echo "Les Héros :\n
+                - Goku : il a 100 PV, et posséde un avantage, le bouclier\n
+                - Vegeta : il possède plus de PV que les autres personnages, soit 120 PV\n
+Les Méchants :\n
+                - Freezer : il a davantage de vie, soit 140 PV \n
+                - Cell : il fait perdre 20 PV à ces adversaires et a 100 PV.\n";
+                $revenir_menu=readline("\nPour revenir sur le Menu merci de taper 'Go' : \n");
+                if ($revenir_menu== "Go"){
+                    popen("cls", "w");
+                }
+                Menu($Commence);
+                break;
+            case "4":
+                echo ("Vous avez quitter le jeu.");
+                break;
+            default:
+        }
+        
+    }
+    
+}
+
+Menu($Commence);
 ?>
