0f084c600b1ddc5ed0f4cfda4c89c49abc159c73
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-10-26 13:55:29 +0200

    Amélioration du menu + choix du perso
    Modification/Amélioration du menu et création d'un fonction Démarrer, qui permet au joueur de choisir son perso en fonction de sa classe.

--- Examen2.php
+++ Examen2.php
@@ -2,23 +2,20 @@
 //Création d'une classe personnage
 class Personnage
 {
-    private $nom;
-    private $puissance;
-    private $pointdevie;
-
-    private $attaque;
+    protected $nom;
+    protected $puissance;
+    protected $pointdevie;
     //Constructeur donnant un nom et un pouvoir
     public function __construct($nom,$puissance,$pointdevie){
         $this->nom=$nom;
         $this->puissance=$puissance;
         $this->pointdevie=$pointdevie;
-        
     }
-    public function afficherNom($nom){
-        return $nom;
+    public function afficherNom(){
+        return $this->nom;
     }
-    public function afficherSante($pointdevie){
-        return $pointdevie;
+    public function afficherSante(){
+        return $this->pointdevie;
     }
 }
 //Création d'une classe Hero avec pour parent la classe personnage
@@ -32,26 +29,37 @@ class Heros extends Personnage
             $this->avantage=$avantage;
         }
     //Fonction pour afficher les différentes parties
-    public function afficherNom($nom){
-        return $nom;
+    public function afficherNom(){
+        return $this->nom;
     }
-    public function afficherSante($pointdevie){
-        return $pointdevie;
+    public function afficherSante(){
+        return $this->pointdevie;
     }
-    public function afficherPuissance($puissance){
-        return $puissance;
+    public function afficherPuissance(){
+        return $this->puissance;
     }
-    public function afficherAvantage($avantage){
-        return $avantage;
+    public function afficherAvantage(){
+        return $this->avantage;
     }
+    public function attaquer(Mechants $cible){
+        $degats = $this->afficherPuissance();
+        $cible->subirDegats($degats);
+    }
+    public function subirDegats($degats) {
+        $this->pointdevie -= $degats;
+        if ($this->pointdevie <= 0) {
+            $this->mourrir();
+        }
+    }
+    public function mourrir() {
+        echo $this->afficherNom() . " est mort.";
+    }
+    
 }
 //Creation de la classe vilain avec pour parent la classe personnage
 class Mechants extends Personnage
 {
     //Définition des variables
-    private $nom;
-    private $puissance;
-    private $pointdevie;
     private $destructeur;
     //Recuperation avec la classe parent et ajout de l'attribut destructeur
     public function __construct($nom,$pointdevie,$puissance,$destructeur){
@@ -59,40 +67,79 @@ class Mechants extends Personnage
             $this->destructeur=$destructeur;
         }
     //Fonctions permettant de renvoyer les différents attributs du vilain créé
-    public function afficherNom($nom){
-        return $nom;
+    public function afficherNom(){
+        return $this->nom;
     }
-    public function afficherSante($pointdevie){
-        return $pointdevie;
+    public function afficherSante(){
+        return $this->pointdevie;
     }
-    public function afficherPouvoir($pouvoir){
-        return $pouvoir;
+    public function afficherPuissance(){
+        return $this->puissance;
     }
-    public function afficherDestructeur($destructeur){
-        return $destructeur;
+    public function afficherDestructeur(){
+        return $this->destructeur;
+    }
+
+    public function attaquer(Heros $cible){ //associe le nom d'une classe à une variable
+        $degats = $this->afficherPuissance();
+        $cible->subirDegats($degats);
+    }
+    
+    public function subirDegats($degats){
+        $this->pointdevie -= $degats;
+        if ($this->pointdevie < 0){
+            $this->mourir();
+        }
+    }
+    
+    public function mourir(){
+        echo $this->afficherNom() . "est mort.\n";
     }
 }
+//Création d'un héro
+// $heros = new Heros("Jean", 100, 800, "toujours plus vite");
+// $mechant = new Mechants("Janne", 100, 100, "Lance des bombes");
+
+// $heros->attaquer($mechant);
+
+// echo $mechant->afficherSante();
+
 //Création d'un héro
 
 //$heros=new Heros($nom,$puissance,$pointdevie,$avantage);
 
-$Goku=new Heros("Goku", "20 points de dégats", 100, "Bouclier");
-$Vegeta = new Heros("Vegeta", "10 points de dégats", 120, "plus de vie");
+$Goku=new Heros("Goku", 20, 100, "Bouclier");
+$Vegeta = new Heros("Vegeta", 10 , 120, "plus de vie");
 
 //$vilain= new Mechants($nom, $puissance,$pointdevie,$destructeur);
 
-$Freezer = new Mechants("Freezer", "10 points de dégats", 140, "plus de vie");
-$Cell = new Mechants("Cell", "15 points de dégats", 100, "plus de dégat");
+$Freezer = new Mechants("Freezer", 10 , 140, "plus de vie");
+$Cell = new Mechants("Cell", 15, 100, "plus de dégat");
 
-
+function Demarrer($Goku,$Vegeta,$Freezer,$Cell){
+    $choix_classe=readline("Tu veux être un Héro ou un Méchant ? (Héro / Méchant) \n");
+    switch ($choix_classe) {
+        case "Héro":
+            //echo"Bonjour";
+            echo $Goku->afficherNom() . "\n" . $Vegeta . "\n";
+            $choix_perso=readline("Quel personnage choisis-tu ?");
+            break;
+        case "Méchant":
+            echo $Freezer . "\n" . $Cell . "\n";
+            $choix_perso=readline("Quel personnage choisis-tu ?");
+            break;
+        default:
+    }
+    
+}
 
 echo "Bonjour à tous, et bienvenue sur 'Dragon Ball Game'\n
     Nous sommes heureuses de vous accueillir pour un combat opposant les Héros et les Méchants de cette série culte.\n
-    Le jeu comporte au minimum 2 joueurs. Le but est de remporté le plus de combat possible pour gagner le jeu.\n
+    Le jeu comporte au minimum 2 joueurs. Le but est de remporter le plus de combat possible pour gagner le jeu.\n
     Bonne chance à tous et n'oubliez pas, 'Les limites existent uniquement si tu le permets'\n";
 
 $Commence=readline("Voulez-vous commencer (Oui / Non)? ");
-function Menu($Commence){
+function Menu($Commence,$Goku,$Vegeta,$Freezer,$Cell){
     
     if ($Commence== "Oui"){
         echo "\nBienvenue dans le menu du jeu. Que voulez-vous faire ?\n
@@ -105,28 +152,38 @@ function Menu($Commence){
         switch ($choix) {
             case "1":
                 echo"Jouer";
+                Demarrer($Goku,$Vegeta,$Freezer,$Cell);
                 break;
             case "2":
-                echo "Le jeu comporte au minimum 2 joueurs. Le but est de remporté le plus de combat possible pour gagner le jeu.\n
+                echo "Le jeu comporte au minimum 2 joueurs. Le but est de remporter le plus de combat possible pour gagner le jeu.\n
 Bonne chance à tous et n'oubliez pas, 'Les limites existent uniquement si tu le permets'\n";
                 $revenir_menu=readline("Pour revenir sur le Menu merci de taper 'Go' : \n");
                 if ($revenir_menu== "Go"){
                     popen("cls", "w");
-                    Menu($Commence);
+                    Menu($Commence,$Goku,$Vegeta,$Freezer,$Cell);
                 }
                 break;
             case "3":
-                echo "Les Héros :\n
+                $perso_hero=readline("Voulez-vous voir les personnages de la classe Héros ou Méchants ? (Héros/Méchants) \n");
+                if ($perso_hero== "Héros"){
+                    echo "Les Héros :\n
                 - Goku : il a 100 PV, et posséde un avantage, le bouclier\n
-                - Vegeta : il possède plus de PV que les autres personnages, soit 120 PV\n
-Les Méchants :\n
+                - Vegeta : il possède plus de PV que les autres personnages, soit 120 PV\n";
+                $revenir_menu=readline("\nPour revenir sur le Menu merci de taper 'Go' : \n");
+                if ($revenir_menu== "Go"){
+                    popen("cls", "w");
+                }
+                Menu($Commence,$Goku,$Vegeta,$Freezer,$Cell);
+                }else{
+                echo "Les Méchants :\n
                 - Freezer : il a davantage de vie, soit 140 PV \n
                 - Cell : il fait perdre 20 PV à ces adversaires et a 100 PV.\n";
                 $revenir_menu=readline("\nPour revenir sur le Menu merci de taper 'Go' : \n");
                 if ($revenir_menu== "Go"){
                     popen("cls", "w");
                 }
-                Menu($Commence);
+                Menu($Commence,$Goku,$Vegeta,$Freezer,$Cell);
+                }
                 break;
             case "4":
                 echo ("Vous avez quitter le jeu.");
@@ -138,7 +195,10 @@ Les Méchants :\n
     
 }
 
-Menu($Commence);
+
+
+Menu($Commence,$Goku,$Vegeta,$Freezer,$Cell);
+?>
 
 
 
